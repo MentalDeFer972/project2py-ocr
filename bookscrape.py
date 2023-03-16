c8bf3d02dfc841b44960ce4953c5442b2814d7c7
joffr <joffreyterrine@protonmail.com>
2023-03-16 11:24:06 +0100

    commit3

--- bookscrape.py
+++ bookscrape.py
@@ -1,33 +1,39 @@
 import requests
 
 from bs4 import BeautifulSoup
+import pandas as pd
 
 
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 response = requests.get(url)
 
+books = []
 
 if response.ok:
     soup = BeautifulSoup(response.text, "html.parser")
+    #product_page_url = soup.find("h3")
     title = soup.find("h1")
-    td = soup.findAll("td")
-    p = soup.findAll("p")
-    a = soup.findAll("a")
+    category = soup.findAll("a")[3]
+    universal_product_code = soup.findAll("td")[0]
+    price_including_tax = soup.findAll("td")[2]
+    price_excluding_tax = soup.findAll("td")[3]
+    number_available = soup.findAll("td")[4]
+    product_description = soup.findAll("p")[3]
+    image_url = soup.findAll("img")[0]
 
-    category = a[3]
-    universal_product_code = td[0]
-    price_including_tax = td[2]
-    price_excluding_tax = td[3]
-    number_available = td[4]
-    product_description = p[3]
-    image_url = soup.findAll("img")
-
+    #print(product_page_url.text)
     print(universal_product_code.text)
     print(title.text)
     print(price_including_tax.text)
     print(price_excluding_tax.text)
     print(product_description.text)
     print(category.text)
+    print(image_url)
+
+
+
+
+
 
 
 
