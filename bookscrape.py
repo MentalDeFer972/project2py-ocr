5c17726d67431b04fb7f35108b6e4e8944add454
joffr <joffreyterrine@protonmail.com>
2023-03-20 11:53:27 +0100

    commit3

--- bookscrape.py
+++ bookscrape.py
@@ -3,32 +3,42 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
-
-url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+url = "https://books.toscrape.com/"
+url_list = []
 response = requests.get(url)
+soup = BeautifulSoup(response.text, "html.parser")
 
-books = []
+category_side = soup.findAll('ul', 'nav nav-list')
+article = soup.findAll('article', 'product_pod')
 
-if response.ok:
-    soup = BeautifulSoup(response.text, "html.parser")
-    #product_page_url = soup.find("h3")
-    title = soup.find("h1")
-    category = soup.findAll("a")[3]
-    universal_product_code = soup.findAll("td")[0]
-    price_including_tax = soup.findAll("td")[2]
-    price_excluding_tax = soup.findAll("td")[3]
-    number_available = soup.findAll("td")[4]
-    product_description = soup.findAll("p")[3]
-    image_url = soup.findAll("img")[0]
+"""for cat in category_side:
+    get_li = cat.findAll('li')
+    for li in get_li:
+        get_a = li.findAll('a')
+        for a in get_a:
+            print(a['href'])"""
 
-    #print(product_page_url.text)
-    print(universal_product_code.text)
-    print(title.text)
-    print(price_including_tax.text)
-    print(price_excluding_tax.text)
-    print(product_description.text)
-    print(category.text)
-    print(image_url)
+
+def tab_catalogue():
+    for a in article:
+        get_div = a.findAll('div')
+        for div in get_div:
+            get_a = div.findAll('a')
+            for a in get_a:
+                url_list.append(url+a['href'])
+
+tab_catalogue()
+
+for urll in url_list:
+    print(urll)
+
+
+
+
+
+
+
+
 
 
 
