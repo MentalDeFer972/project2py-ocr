98a6253839762118a163a591e8c7eaac25f00db1
joffr <joffreyterrine@protonmail.com>
2023-03-20 12:25:43 +0100

    commit4

--- bookscrape.py
+++ bookscrape.py
@@ -30,7 +30,32 @@ def tab_catalogue():
 tab_catalogue()
 
 for urll in url_list:
-    print(urll)
+    response2 = requests.get(urll)
+    soup2 = BeautifulSoup(response2.text, 'html.parser')
+
+    product_page_url = urll
+    title = soup2.find("h1")
+    category = soup2.findAll("a")[3]
+    universal_product_code = soup2.findAll("td")[0]
+    price_including_tax = soup2.findAll("td")[2]
+    price_excluding_tax = soup2.findAll("td")[3]
+    number_available = soup2.findAll("td")[5]
+    product_description = soup2.findAll("p")[3]
+    image_url = soup2.findAll("img")[0]
+    review_rating = soup2.findAll("td")[6]
+
+    print(product_page_url)
+    print(universal_product_code.text)
+    print(title.text)
+    print(price_including_tax.text)
+    print(price_excluding_tax.text)
+    print(product_description.text)
+    print(category.text)
+    print(number_available.text)
+    print(review_rating.text)
+    print(image_url['src'])
+
+
 
 
 
